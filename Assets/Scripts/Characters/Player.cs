3f718fd6c891674f88d084e8eaee589a0440509c
GorayaU <udeyveer.goraya@ontariotechu.net>
2023-09-29 21:34:49 -0400

    input system
    Created a movement system as well as a scaling system

---
+++ Assets/Scripts/Characters/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.iOS;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    private Vector3 moveDir;
+    void Start()
+    {
+        InputManager.Init(myPlayer:this);
+        InputManager.GameMode();
+    }
+
+    void Update()
+    {
+        transform.position += speed * Time.deltaTime * moveDir;
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.localScale += new Vector3(1, 1, 1);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            transform.localScale -= new Vector3(1, 1, 1);
+        }
+        else if (Input.GetMouseButtonDown(2))
+        {
+            transform.localScale = new(1, 1, 1);
+        }
+
+
+    }
+    public void SetMovementDirection(Vector3 newDirection)
+    {
+        moveDir = newDirection;
+    }
+}
